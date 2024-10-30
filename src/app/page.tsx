88a2376bb4f87914e8bcac5cb3aa3be50a8384e5
Durga rao Ganta <gantadurgarao304@gmail.com>
2024-10-30 23:44:39 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+/** @format */
+
+export default function Home() {
+  return (
+    <div className='flex items-center justify-center w-screen h-screen'>
+      <p>{`We're under construction. Please check back for an update soon.`}</p>
+    </div>
+  );
+}
